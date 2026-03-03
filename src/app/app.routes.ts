d2a863351e3ff57310ce19392a4a96203081826f
Alex Garcia <159174339+alexgcl@users.noreply.github.com>
2026-03-03 14:44:43 +0000

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+ path: 'movies',
+ loadChildren: () => import('./features/movies/movies.routes').then(m => m.moviesRoutes)
+ },
+ {
+ path: '',
+ redirectTo: 'movies',
+ pathMatch: 'full'
+ }
+];
