67c3751060a98df5394143c684dc39a085d4ca29
Alex Garcia <159174339+alexgcl@users.noreply.github.com>
2026-03-09 08:52:51 +0000

    Correcció 4

--- src/app/features/movies/services/movies.service.ts
+++ src/app/features/movies/services/movies.service.ts
@@ -1,6 +1,5 @@
 import { Injectable, signal, computed, inject } from '@angular/core';
 import { Apollo } from 'apollo-angular';
-import { ApolloQueryResult } from '@apollo/client';
 import { MovieData, MovieItem } from '../models/movies.model';
 import { GET_MOVIES, ADD_MOVIE, DELETE_MOVIE, DELETE_ALL_MOVIES } from '../graphql/movies.graphql';
 
@@ -10,14 +9,18 @@ import { GET_MOVIES, ADD_MOVIE, DELETE_MOVIE, DELETE_ALL_MOVIES } from '../graph
 
 export class MoviesService {
   private apollo = inject(Apollo);
+
   private _movies = signal<MovieItem[]>([]);
   private _isLoading = signal(false);
+
   // Expose readonly signals
   movies = this._movies.asReadonly();
   isLoading = this._isLoading.asReadonly();
+
   // Computed signal for total movies count
   moviesCount = computed(() => this._movies().length);
   deletingIds = computed(() => new Set(this._movies().filter((m: MovieItem) => m.isDeleting).map((m: MovieItem) => m.data.id)));
+
   private _moviesListWatchQuery = this.apollo.watchQuery<{ movies: MovieData[] }>({ query: GET_MOVIES });
   private _moviesAddMutation = (title: string) => this.apollo.mutate<{ addMovie: MovieData }>({
     mutation: ADD_MOVIE, variables: { title }, refetchQueries: [{ query: GET_MOVIES }]
@@ -28,9 +31,11 @@ export class MoviesService {
   private _moviesDeleteAllMutation = () => this.apollo.mutate<{ deleteAllMovies: boolean }>({
     mutation: DELETE_ALL_MOVIES, refetchQueries: [{ query: GET_MOVIES }]
   });
+
   constructor() {
     this.loadMovies();
   }
+  
   private loadMovies(): void {
   console.log('Loading movies...');
   this._isLoading.set(true);
