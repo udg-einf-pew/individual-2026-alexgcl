48b8077c929ead50af6ba63c0b45235209e19bcb
Alex Garcia <159174339+alexgcl@users.noreply.github.com>
2026-03-09 08:37:54 +0000

    Correcció 2

--- server/src/resolvers/movies.resolver.ts
+++ server/src/resolvers/movies.resolver.ts
@@ -1,36 +1,27 @@
-import { Movie } from '../types'
-import { MoviesService } from '../services/movies.service'
+import {Movie} from '../types'
+import {MoviesService} from '../services/movies.service'
 
 export class MoviesResolver {
     private moviesService: MoviesService
+
     constructor() {
         this.moviesService = new MoviesService()
     }
+    
     getResolvers() {
         return {
             Query: {
-                movies: (): Movie[] => {
-                    try {
-                        return this.moviesService.getMovies() ?? []
-                    } catch (err) {
-                        console.error('getMovies error:', err)
-                        return []
-                    }
-                }
+                movies: (): Movie[] => this.moviesService.getMovies()
             },
             Mutation: {
-                addMovie: async (_parent: unknown, args: { title: string }): Promise<Movie> => {
-                    try {
-                        return await this.moviesService.addMovie(args?.title ?? '')
-                    } catch (err) {
-                        console.error('addMovie error:', err)
-                        throw err
-                    }
-                },
-                deleteMovie: (_parent: unknown, args: { id: string }): boolean =>
-                    this.moviesService.deleteMovie(args.id),
-                deleteAllMovies: (): boolean =>
-                    this.moviesService.deleteAllMovies()
+                addMovie: async (_parent: unknown, args: {title: string}): Promise<Movie> =>
+                await this.moviesService.addMovie(args.title),
+
+                deleteMovie: async (_parent: unknown, args: {id: string}): Promise<boolean> => 
+                await this.moviesService.deleteMovie(args.id),
+                
+                deleteAllMovies: async (): Promise<boolean> => 
+                await this.moviesService.deleteAllMovies()
             }
         }
     }
